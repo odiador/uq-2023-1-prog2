59960ccb5b19c25d40ca1e1047c1641802197df8
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-03-16 13:32:38 -0500

    Update Lending.java
    Created toString(), hashCode() and equals() method

--- Parcial 1/ws_parcial 1/src/co/edu/uniquindio/parcial1/model/Lending.java
+++ Parcial 1/ws_parcial 1/src/co/edu/uniquindio/parcial1/model/Lending.java
@@ -254,4 +254,36 @@ public class Lending {
         }
         return false;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((code == null) ? 0 : code.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Lending other = (Lending) obj;
+        if (code == null) {
+            if (other.code != null)
+                return false;
+        } else if (!code.equals(other.code))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Lending [date=" + date + ", deliveryDate=" + deliveryDate + ", code=" + code + ", employer=" + employer
+                + ", isEnded=" + isEnded + ", lendingDetailList=" + lendingDetailList + "]";
+    }
+    
 }
