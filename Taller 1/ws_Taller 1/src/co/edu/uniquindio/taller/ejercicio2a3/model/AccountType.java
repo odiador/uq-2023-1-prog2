5fe5deb4b566c0ce9ecd4c9ead4da3fd685fc111
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-03-23 21:26:40 -0500

    Update AccountType.java

--- Taller 1/ws_Taller 1/src/co/edu/uniquindio/taller/ejercicio2a3/model/AccountType.java
+++ Taller 1/ws_Taller 1/src/co/edu/uniquindio/taller/ejercicio2a3/model/AccountType.java
@@ -4,12 +4,6 @@ package co.edu.uniquindio.taller.ejercicio2a3.model;
  * Son los tipos de cuenta posibles
  */
 public enum AccountType {
-    /**
-     * Tipo de cuenta ahorros
-     */
     AHORROS,
-    /**
-     * Tipo de cuenta corriente
-     */
     CORRIENTE
 }
