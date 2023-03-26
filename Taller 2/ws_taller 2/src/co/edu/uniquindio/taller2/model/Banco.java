e46530de20d7cd3e4d1729550ea05d0e702d6031
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-03-25 19:10:12 -0500

    Update Banco.java
    Added the relation between Banco and Cuenta and added new CRUD methods

--- Taller 2/ws_taller 2/src/co/edu/uniquindio/taller2/model/Banco.java
+++ Taller 2/ws_taller 2/src/co/edu/uniquindio/taller2/model/Banco.java
@@ -1,10 +1,14 @@
 package co.edu.uniquindio.taller2.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
 
 	protected String nombre;
 	protected String direccion;
 	protected String codigo;
+	protected List<Cuenta> listaCuentas = new ArrayList<Cuenta>();
 
 	public Banco(String nombre, String direccion, String codigo) {
 		this.nombre = nombre;
@@ -13,6 +17,39 @@ public class Banco {
 	}
 
 	public Banco() {
+	}
+
+	public Cuenta buscarCuenta(String codigo) {
+		return getListaCuentas().stream().filter(cadaCuenta -> cadaCuenta.getCodigo().equals(codigo)).findFirst()
+				.orElse(null);
+	}
+
+	public void agregarCuentaAhorros(String codigo, float saldo, float tasaAnual) throws CuentaException {
+		if (validarCuenta(codigo)) {
+			throw new CuentaException("La cuenta ya existe (" + codigo + ")");
+		}
+		getListaCuentas().add(new CuentaAhorros(codigo, saldo, tasaAnual));
+	}
+
+	public void agregarCuentaCorriente(String codigo, float saldo, float tasaAnual) throws CuentaException {
+		if (validarCuenta(codigo)) {
+			throw new CuentaException("La cuenta ya existe (" + codigo + ")");
+		}
+		getListaCuentas().add(new CuentaCorriente(codigo, saldo, tasaAnual));
+	}
+
+	public void eliminarCuenta(String codigo) throws CuentaException {
+		if (!validarCuenta(codigo)) {
+			throw new CuentaException("La cuenta no existe (" + codigo + ")");
+		}
+	}
+
+	public List<Cuenta> getListaCuentas() {
+		return listaCuentas;
+	}
+
+	public boolean validarCuenta(String codigo) {
+		return buscarCuenta(codigo) != null;
 	}
 
 	public String getNombre() {
