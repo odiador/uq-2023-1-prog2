30aa6b90c8d25dab543eccb4e9ebdb3e53578788
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-03-31 08:28:50 -0500

    Update Documento.java
    Added the attributes of the class

--- Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/Documento.java
+++ Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/Documento.java
@@ -1,5 +1,44 @@
 package co.edu.uniquindio.centroimpresion.model;
 
 public class Documento {
+	private String code;
+	private String titulo;
+	private int prioridad;
+
+	/**
+	 *
+	 * @param code
+	 * @param titulo
+	 * @param prioridad
+	 */
+	public Documento(String code, String titulo, int prioridad) {
+		this.code = code;
+		this.titulo = titulo;
+		this.prioridad = prioridad;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getPrioridad() {
+		return prioridad;
+	}
+
+	public void setPrioridad(int prioridad) {
+		this.prioridad = prioridad;
+	}
 
 }
