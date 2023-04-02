9d34c6d387416c1f26a62669b7971cf755ca8fa4
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-01 22:09:37 -0500

    Update Documento.java
    Added time attributes

--- Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/Documento.java
+++ Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/Documento.java
@@ -1,31 +1,40 @@
 package co.edu.uniquindio.centroimpresion.model;
 
+import java.time.LocalDateTime;
+
 public class Documento implements Comparable<Documento> {
-	private String code;
+	private final String code;
 	private String titulo;
 	private int prioridad;
-	private String cuerpo;
+	private String contenido;
+	private LocalDateTime fechaAgregado;
+	private LocalDateTime fechaImpresion = null;
 
 	/**
 	 *
 	 * @param code
 	 * @param titulo
 	 * @param prioridad
-	 * @param cuerpo
+	 * @param contenido
 	 */
-	public Documento(String code, String titulo, int prioridad, String cuerpo) {
+	public Documento(String code, String titulo, int prioridad, String contenido, LocalDateTime fechaAgregado) {
 		this.code = code;
 		this.titulo = titulo;
 		this.prioridad = prioridad;
-		this.cuerpo = cuerpo;
+		this.contenido = contenido;
+		this.fechaAgregado = fechaAgregado;
+	}
+
+	public Documento() {
+		this.code = "";
+	}
+
+	public boolean estadoImpresion() {
+		return fechaImpresion != null;
 	}
 
 	public String getCode() {
 		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
 	}
 
 	public String getTitulo() {
@@ -44,12 +53,28 @@ public class Documento implements Comparable<Documento> {
 		this.prioridad = prioridad;
 	}
 
-	public String getCuerpo() {
-		return cuerpo;
+	public String getContenido() {
+		return contenido;
 	}
 
-	public void setCuerpo(String cuerpo) {
-		this.cuerpo = cuerpo;
+	public void setContenido(String cuerpo) {
+		this.contenido = cuerpo;
+	}
+
+	public LocalDateTime getFechaAgregado() {
+		return fechaAgregado;
+	}
+
+	public void setFechaAgregado(LocalDateTime fechaAgregado) {
+		this.fechaAgregado = fechaAgregado;
+	}
+
+	public LocalDateTime getFechaImpresion() {
+		return fechaImpresion;
+	}
+
+	public void setFechaImpresion(LocalDateTime fechaImpresion) {
+		this.fechaImpresion = fechaImpresion;
 	}
 
 	@Override
