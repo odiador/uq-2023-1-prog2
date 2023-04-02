c04ed17eaf74256a1c9c89d3b470fe3da54c16b9
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-01 20:41:41 -0500

    Update Documento.java
    Added util methods and implemented a way of compare 2 documents by priority

--- Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/Documento.java
+++ Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/Documento.java
@@ -1,6 +1,6 @@
 package co.edu.uniquindio.centroimpresion.model;
 
-public class Documento {
+public class Documento implements Comparable<Documento> {
 	private String code;
 	private String titulo;
 	private int prioridad;
@@ -39,6 +39,40 @@ public class Documento {
 
 	public void setPrioridad(int prioridad) {
 		this.prioridad = prioridad;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Documento other = (Documento) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		return true;
+	}
+
+	public int compararPorTitulo(Documento o) {
+		return getTitulo().compareTo(o.getTitulo());
+	}
+
+	@Override
+	public int compareTo(Documento o) {
+		return o.getPrioridad() - getPrioridad();
 	}
 
 }
