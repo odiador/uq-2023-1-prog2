920407ba8ff526dac5d1ea35be61b89ccb7b341a
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-02 19:48:20 -0500

    Update Main.java
    Now uses the PanelPrincipal class

--- Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/application/Main.java
+++ Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/application/Main.java
@@ -1,16 +1,17 @@
 package co.edu.uniquindio.centroimpresion.application;
 
+import co.edu.uniquindio.centroimpresion.view.OpcionPrincipal;
+import co.edu.uniquindio.centroimpresion.view.PanelPrincipal;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root, 400, 400);
+			PanelPrincipal root = new PanelPrincipal("Juan Manuel Amador Roa", OpcionPrincipal.values());
+			Scene scene = new Scene(root, 800, 500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
