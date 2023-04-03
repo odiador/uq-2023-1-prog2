2fc834c0a6a12db3cd31aa0958a185c7c7592c0f
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-02 19:45:42 -0500

    Create Header.java

---
+++ Taller 3/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/Header.java
@@ -0,0 +1,44 @@
+package co.edu.uniquindio.centroimpresion.view;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class Header extends BorderPane {
+	private String nombre;
+	private Label lblNombre;
+
+	/**
+	 * Es el constructor de la clase Header
+	 *
+	 * @param nombre
+	 */
+	public Header(String nombre) {
+		this.nombre = nombre;
+		initComp();
+	}
+
+	/**
+	 * Inicializa los componentes del panel y directamente también los agrega
+	 */
+	private void initComp() {
+		lblNombre = new Label(nombre);
+		setCenter(lblNombre);
+		setPrefHeight(200d);
+	}
+
+	public Label getLblNombre() {
+		return lblNombre;
+	}
+
+	public void setLblNombre(Label lblNombre) {
+		this.lblNombre = lblNombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String name) {
+		this.nombre = name;
+	}
+}
