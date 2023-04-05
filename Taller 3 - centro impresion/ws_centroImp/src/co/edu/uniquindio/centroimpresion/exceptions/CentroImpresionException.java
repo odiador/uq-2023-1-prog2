86ea369c615f4a54ad6c7f1bdf1ac9bd070f17a4
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-05 14:45:38 -0500

    Update CentroImpresionException.java
    Added new class attribute

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/exceptions/CentroImpresionException.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/exceptions/CentroImpresionException.java
@@ -3,11 +3,15 @@ package co.edu.uniquindio.centroimpresion.exceptions;
 public class CentroImpresionException extends Exception {
 
 	private TipoCentroException tipoException;
+	private Class<?> clase;
 
-	public CentroImpresionException(TipoCentroException tipoException) {
-		super(tipoException.getMsg());
+	public CentroImpresionException(TipoCentroException tipoException, Class<?> clase) {
+		super(tipoException.getMsg() + " (" + clase.getSimpleName() + ")");
 		this.tipoException = tipoException;
+	}
 
+	public Class<?> getClase() {
+		return clase;
 	}
 
 	public TipoCentroException getTipoException() {
