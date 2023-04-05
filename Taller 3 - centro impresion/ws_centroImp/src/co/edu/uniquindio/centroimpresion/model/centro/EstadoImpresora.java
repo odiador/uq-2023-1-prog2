7a4f14b17acba531d38455fc92ede7ca415567a2
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-05 17:48:58 -0500

    Update EstadoImpresora.java
    Added a new attribute

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/centro/EstadoImpresora.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/centro/EstadoImpresora.java
@@ -2,6 +2,16 @@ package co.edu.uniquindio.centroimpresion.model.centro;
 
 import java.io.Serializable;
 
-public enum EstadoImpresora  implements Serializable {
-	ACTIVO, INACTIVO, DESCONECTADO, VACIO, MANTENIMIENTO;
+public enum EstadoImpresora implements Serializable {
+	ACTIVO("Activo"), INACTIVO("Inactivo"), DESCONECTADO("Desconectado"), MANTENIMIENTO("En Mantenimiento");
+	private String texto;
+
+	private EstadoImpresora(String texto) {
+		this.texto = texto;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
 }
