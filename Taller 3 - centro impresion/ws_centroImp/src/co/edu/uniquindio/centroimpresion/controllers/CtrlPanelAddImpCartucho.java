bb73a68b907e1c024f4d61d35abcd3dae3f20c10
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-05 18:22:35 -0500

    Update CtrlPanelAddImpCartucho.java
    Added a method that adds the Impresora and another that obtain it with the information

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/controllers/CtrlPanelAddImpCartucho.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/controllers/CtrlPanelAddImpCartucho.java
@@ -1,11 +1,81 @@
 package co.edu.uniquindio.centroimpresion.controllers;
 
+import co.edu.uniquindio.centroimpresion.exceptions.CentroImpresionException;
 import co.edu.uniquindio.centroimpresion.exceptions.FueraRangoException;
 import co.edu.uniquindio.centroimpresion.exceptions.ObjectNotExists;
 import co.edu.uniquindio.centroimpresion.exceptions.TextIsEmptyException;
+import co.edu.uniquindio.centroimpresion.model.archivos.SerializedData;
 import co.edu.uniquindio.centroimpresion.model.centro.EstadoImpresora;
+import co.edu.uniquindio.centroimpresion.model.centro.ImpresoraCartucho;
 
 public class CtrlPanelAddImpCartucho {
+
+	/**
+	 *
+	 * @param code
+	 * @param marca
+	 * @param estadoString
+	 * @param esAColor
+	 * @param paginasPorMinutoString
+	 * @param capacidadCartuchoString
+	 * @param desgasteCartuchoString
+	 * @throws CentroImpresionException
+	 * @throws TextIsEmptyException
+	 * @throws ObjectNotExists
+	 * @throws NumberFormatException
+	 * @throws FueraRangoException
+	 */
+	public static void agregarImpresoraCartucho(String code, String marca, String estadoString, boolean esAColor,
+			String paginasPorMinutoString, String capacidadCartuchoString, String desgasteCartuchoString)
+			throws CentroImpresionException, TextIsEmptyException, ObjectNotExists, NumberFormatException,
+			FueraRangoException {
+		ImpresoraCartucho impresoraCartucho = obtenerImpresoraCartucho(code, marca, estadoString, esAColor,
+				paginasPorMinutoString, capacidadCartuchoString, desgasteCartuchoString);
+		SerializedData data = new SerializedData();
+		if (impresoraCartucho.exists()) {
+			data.getCentroImpresion().agregarImpresoraCartucho(impresoraCartucho.getCode(),
+					impresoraCartucho.getMarca(), impresoraCartucho.getEstado(), impresoraCartucho.isEsAColor(),
+					impresoraCartucho.getPaginasPorMinuto(), impresoraCartucho.getCapacidadCartucho(),
+					impresoraCartucho.getCapacidadCartucho());
+			data.updateCentroImpresion();
+		}
+	}
+
+	/**
+	 *
+	 * @param code
+	 * @param marca
+	 * @param estadoString
+	 * @param esAColor
+	 * @param paginasPorMinutoString
+	 * @param capacidadCartuchoString
+	 * @param desgasteCartuchoString
+	 * @return
+	 * @throws TextIsEmptyException
+	 * @throws ObjectNotExists
+	 * @throws NumberFormatException
+	 * @throws FueraRangoException
+	 */
+	private static ImpresoraCartucho obtenerImpresoraCartucho(String code, String marca, String estadoString,
+			boolean esAColor, String paginasPorMinutoString, String capacidadCartuchoString,
+			String desgasteCartuchoString)
+			throws TextIsEmptyException, ObjectNotExists, NumberFormatException, FueraRangoException {
+		throwIfEmpty(code, "codigo");
+		throwIfEmpty(marca, "marca");
+		throwIfEmpty(estadoString, "estado");
+		throwIfEmpty(paginasPorMinutoString, "paginas por minuto");
+		throwIfEmpty(capacidadCartuchoString, "capacidad de cartucho");
+		throwIfEmpty(desgasteCartuchoString, "descaste de cartucho");
+
+		EstadoImpresora estadoImpresora = obtenerEstadoImpresora(estadoString);
+		double paginasPorMinuto = obtenerPagPerMinute(paginasPorMinutoString);
+		double capacidadCartucho = obtenerCapacidad(capacidadCartuchoString);
+		double desgasteCartucho = obtenerDesgaste(desgasteCartuchoString);
+
+		ImpresoraCartucho impresoraCartucho = new ImpresoraCartucho(code, marca, estadoImpresora, esAColor,
+				paginasPorMinuto, capacidadCartucho, desgasteCartucho);
+		return impresoraCartucho;
+	}
 
 	private static void throwIfEmpty(String texto, String tipo) throws TextIsEmptyException {
 		if (texto.isEmpty())
