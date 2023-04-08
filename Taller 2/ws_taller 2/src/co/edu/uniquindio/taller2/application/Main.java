d2ef741726383a8722d8a80530fdeb36b1aa37fe
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-08 18:13:02 -0500

    Update Main.java
    Added a CuentaAhorros and implemented the methods consignarDinero and retirarDinero

--- Taller 2/ws_taller 2/src/co/edu/uniquindio/taller2/application/Main.java
+++ Taller 2/ws_taller 2/src/co/edu/uniquindio/taller2/application/Main.java
@@ -1,7 +1,25 @@
 package co.edu.uniquindio.taller2.application;
 
+import co.edu.uniquindio.taller2.exceptions.CuentaException;
+import co.edu.uniquindio.taller2.model.CuentaAhorros;
+
 public class Main {
 	public static void main(String[] args) {
-
+		System.out.println("Crea una cuenta con código AB001, saldo $1000 y tasa anual de $10000");
+		CuentaAhorros cuentaAhorros = new CuentaAhorros("AB001", 1000f, 10000f);
+		System.out.println("La cuenta no está activa (su saldo es menor a 10000, "
+				+ "entonces no se va a poder ni agregar ni retirar saldo");
+		System.out.println("Intenta consignar 10000:");
+		try {
+			cuentaAhorros.consignarDinero(10000f);
+		} catch (CuentaException e) {
+			System.err.println(e.getMessage());
+		}
+		System.out.println("Intenta retirar 10000:");
+		try {
+			cuentaAhorros.retirarDinero(10000f);
+		} catch (CuentaException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
