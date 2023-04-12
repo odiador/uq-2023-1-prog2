a724ca5e70258e6611aacf1e6d32e7aa64574af0
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-12 18:20:51 -0500

    Create CtrlEliminarDocumento.java
    Added a Controller if wants to delete a document

---
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/controllers/CtrlEliminarDocumento.java
@@ -0,0 +1,28 @@
+package co.edu.uniquindio.centroimpresion.controllers;
+
+import co.edu.uniquindio.centroimpresion.exceptions.CentroImpresionException;
+import co.edu.uniquindio.centroimpresion.exceptions.TextIsEmptyException;
+import co.edu.uniquindio.centroimpresion.model.archivos.SerializedData;
+import co.edu.uniquindio.centroimpresion.view.util.Utility;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class CtrlEliminarDocumento {
+	public static void eliminarDocumento(String code) {
+		try {
+			eliminarDocumentoThrow(code);
+			new Alert(AlertType.INFORMATION, "El documento con codigo " + code + " fue eliminado correctamente").show();
+		} catch (TextIsEmptyException e) {
+			new Alert(AlertType.WARNING, "Rellena todos los campos (" + e.getTipoTexto() + ")").show();
+		} catch (CentroImpresionException e) {
+			new Alert(AlertType.WARNING, e.getMessage()).show();
+		}
+	}
+
+	public static void eliminarDocumentoThrow(String code) throws TextIsEmptyException, CentroImpresionException {
+		Utility.throwIfEmpty(code, "codigo");
+		SerializedData data = new SerializedData();
+		data.getCentroImpresion().deleteDocumento(code);
+		data.updateCentroImpresion();
+	}
+}
