316990ae3e5c2c6f0374ac88a7a1592312348475
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-12 17:47:39 -0500

    Update PanelRemoveDoc.java
    Created design of panel

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/remove/PanelRemoveDoc.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/remove/PanelRemoveDoc.java
@@ -2,12 +2,34 @@ package co.edu.uniquindio.centroimpresion.view.remove;
 
 import co.edu.uniquindio.centroimpresion.view.custom.PanelConVolver;
 import co.edu.uniquindio.centroimpresion.view.custom.PanelMenuOpcionObjetos;
+import co.edu.uniquindio.centroimpresion.view.util.Utility;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class PanelRemoveDoc extends PanelConVolver {
 	private PanelMenuOpcionObjetos panel;
 
 	public PanelRemoveDoc(PanelMenuOpcionObjetos panel) {
 		this.panel = panel;
+		initComponents();
+	}
+
+	public void initComponents() {
+		VBox vBox = new VBox(30);
+		Label bEliminar = new Label("Eliminar Documento");
+		TextField tfCode = new TextField();
+		BorderPane panelCase = new BorderPane(bEliminar);
+
+		vBox.setId("centered-box");
+		panelCase.setId("btn-case");
+		bEliminar.setId("btn");
+
+		vBox.getChildren().add(Utility.generarHBox("Escribe el codigo del documento a eliminar: ", tfCode));
+		vBox.getChildren().add(panelCase);
+
+		setCenter(vBox);
 	}
 
 	@Override
