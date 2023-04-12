32c908a7cfc87f4de975ef164ba10306e0ee4d4f
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-12 16:24:26 -0500

    Added a new util static attribute called CSS

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/application/Main.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/application/Main.java
@@ -1,15 +1,20 @@
 package co.edu.uniquindio.centroimpresion.application;
 
+import java.net.URL;
+
 import co.edu.uniquindio.centroimpresion.model.scenes.EscenaMenu;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	public static URL css;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			EscenaMenu scene = EscenaMenu.obtenerEscenaJm(primaryStage);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			css = getClass().getResource("application.css");
+			scene.getStylesheets().add(css.toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
