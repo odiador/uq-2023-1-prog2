c72ff080573fe9546245bcb93f295c91f8a3f3d9
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-13 15:28:39 -0500

    Added a new Pane to see documents

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/controllers/CtrlSeeDocs.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/controllers/CtrlSeeDocs.java
@@ -1,14 +1,20 @@
 package co.edu.uniquindio.centroimpresion.controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import co.edu.uniquindio.centroimpresion.model.centro.Documento;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.util.Callback;
 
 public class CtrlSeeDocs {
-	public static Callback<TableColumn<Documento, String>, TableCell<Documento, String>> obtenerValoresContenido() {
+	public static Callback<TableColumn<Documento, String>, TableCell<Documento, String>> obtenerCallbackContenido() {
 		return new Callback<TableColumn<Documento, String>, TableCell<Documento, String>>() {
 
 			final @Override public TableCell<Documento, String> call(TableColumn<Documento, String> param) {
@@ -29,6 +35,32 @@ public class CtrlSeeDocs {
 				};
 				return cell;
 			}
+		};
+	}
+
+	public static Callback<CellDataFeatures<Documento, String>, ObservableValue<String>> obtenerCallbackTitulo() {
+		return data -> new ReadOnlyStringWrapper(data.getValue().getTitulo());
+	}
+
+	public static Callback<CellDataFeatures<Documento, String>, ObservableValue<String>> obtenerCallbackCodigo() {
+		return data -> new ReadOnlyStringWrapper(data.getValue().getCode());
+	}
+
+	public static Callback<CellDataFeatures<Documento, String>, ObservableValue<String>> obtenerCallbackPrioridad() {
+		return data -> new ReadOnlyStringWrapper(data.getValue().getPrioridad() + "");
+	}
+
+	public static Callback<CellDataFeatures<Documento, String>, ObservableValue<String>> obtenerCallbackFechaAgregado() {
+		return data -> new ReadOnlyStringWrapper(
+				data.getValue().getFechaAgregado().format(DateTimeFormatter.ofPattern("HH:mm:ss, dd/MM/yy")));
+	}
+
+	public static Callback<CellDataFeatures<Documento, String>, ObservableValue<String>> obtenerCallbackFechaImpresion() {
+		return data -> {
+			LocalDateTime fechaImpresion = data.getValue().getFechaImpresion();
+			return new ReadOnlyStringWrapper(
+					fechaImpresion != null ? fechaImpresion.format(DateTimeFormatter.ofPattern("HH:mm:ss, dd/MM/yy"))
+							: "N/A");
 		};
 	}
 }
--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/see/PanelSeeDocs.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/see/PanelSeeDocs.java
@@ -1,17 +1,57 @@
 package co.edu.uniquindio.centroimpresion.view.see;
 
+import java.util.List;
+
+import co.edu.uniquindio.centroimpresion.controllers.CtrlSeeDocs;
+import co.edu.uniquindio.centroimpresion.model.archivos.SerializedData;
+import co.edu.uniquindio.centroimpresion.model.centro.Documento;
 import co.edu.uniquindio.centroimpresion.view.custom.PanelConVolver;
 import co.edu.uniquindio.centroimpresion.view.custom.PanelMenuOpcionObjetos;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class PanelSeeDocs extends PanelConVolver {
 	private PanelMenuOpcionObjetos panel;
 
 	public PanelSeeDocs(PanelMenuOpcionObjetos panel) {
 		this.panel = panel;
+		initComponents();
+	}
+
+	public void initComponents() {
+		TableView<Documento> tableView = new TableView<Documento>();
+		TableColumn<Documento, String> colPrioridad = new TableColumn<Documento, String>("Prioridad");
+		TableColumn<Documento, String> colCodigo = new TableColumn<Documento, String>("Codigo");
+		TableColumn<Documento, String> colTitulo = new TableColumn<Documento, String>("Titulo");
+		TableColumn<Documento, String> colContenido = new TableColumn<>("Contenido");
+		TableColumn<Documento, String> colFechaAgregado = new TableColumn<>("Fecha Agregado");
+		TableColumn<Documento, String> colFechaImpreso = new TableColumn<>("Fecha Impreso");
+
+		colPrioridad.setCellValueFactory(CtrlSeeDocs.obtenerCallbackPrioridad());
+		colTitulo.setCellValueFactory(CtrlSeeDocs.obtenerCallbackTitulo());
+		colCodigo.setCellValueFactory(CtrlSeeDocs.obtenerCallbackCodigo());
+		colFechaAgregado.setCellValueFactory(CtrlSeeDocs.obtenerCallbackFechaAgregado());
+		colFechaImpreso.setCellValueFactory(CtrlSeeDocs.obtenerCallbackFechaImpresion());
+		colContenido.setCellFactory(CtrlSeeDocs.obtenerCallbackContenido());
+
+		tableView.getColumns().add(colCodigo);
+		tableView.getColumns().add(colPrioridad);
+		tableView.getColumns().add(colTitulo);
+		tableView.getColumns().add(colFechaAgregado);
+		tableView.getColumns().add(colFechaImpreso);
+		tableView.getColumns().add(colContenido);
+
+		SerializedData data = new SerializedData();
+		List<Documento> listaDocumentos = data.getCentroImpresion().getListaDocumentos();
+		listaDocumentos.sort(Documento::compareCodeTo);
+		tableView.setItems(FXCollections.observableArrayList(listaDocumentos));
+		setCenter(tableView);
 	}
 
 	@Override
 	public void volverPresionado() {
 		panel.initComp();
 	}
+
 }
