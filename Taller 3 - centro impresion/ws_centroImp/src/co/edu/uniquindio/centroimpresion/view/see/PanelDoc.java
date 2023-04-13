0ebc705b1d6840a603322f7a5b1cb33e0fb7c5d3
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-13 17:10:44 -0500

    Update PanelDoc.java

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/see/PanelDoc.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/see/PanelDoc.java
@@ -4,10 +4,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import co.edu.uniquindio.centroimpresion.model.centro.Documento;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 public class PanelDoc extends VBox {
 	private Documento documento;
@@ -18,13 +18,22 @@ public class PanelDoc extends VBox {
 	}
 
 	public void initComp() {
-		Label codigo = new Label(documento.getCode());
-		Label titulo = new Label(documento.getTitulo());
-		Label prioridad = new Label(String.valueOf(documento.getPrioridad()));
-		Label contenido = new Label(documento.getContenido());
+		setId("centered-box");
+
+		Label codigo = new Label("Codigo: " + documento.getCode());
+		Label titulo = new Label("Titulo: " + documento.getTitulo());
+		Label prioridad = new Label("Prioridad: " + String.valueOf(documento.getPrioridad()));
+		Label contenido = new Label("Ver Contenido");
 		LocalDateTime momentoAgregado = documento.getFechaAgregado();
-		Label fechaAgregado = new Label(momentoAgregado.format(DateTimeFormatter.ofPattern("HH:mm:ss, dd/MM/yy")));
-		Label yaFueImpreso = new Label(documento.getFechaImpresion() != null ? "Si" : "No");
+		DateTimeFormatter pattern = DateTimeFormatter.ofPattern("HH:mm:ss, dd/MM/yy");
+		Label fechaAgregado = new Label("Fecha agregado: " + momentoAgregado.format(pattern));
+		LocalDateTime fechaImpresion = documento.getFechaImpresion();
+		Label yaFueImpreso = new Label(
+				"Fecha de Impresion: " + (fechaImpresion != null ? fechaImpresion.format(pattern) : "N/A"));
+
+		contenido.setId("btn-tabla");
+		contenido.setOnMouseReleased(e -> new Alert(AlertType.INFORMATION, documento.getContenido()).show());
+
 		getChildren().addAll(codigo, titulo, prioridad, contenido, fechaAgregado, yaFueImpreso);
 	}
 
