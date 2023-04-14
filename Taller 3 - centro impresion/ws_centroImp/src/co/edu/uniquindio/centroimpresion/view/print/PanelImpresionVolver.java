cfe72220d42e04a1758d1103d9f31dcc78110f46
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-14 15:32:32 -0500

    Create PanelImpresionVolver.java

---
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/print/PanelImpresionVolver.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.centroimpresion.view.print;
+
+import co.edu.uniquindio.centroimpresion.model.centro.Documento;
+import co.edu.uniquindio.centroimpresion.view.custom.PanelConVolver;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PanelImpresionVolver extends PanelConVolver {
+	private Stage stage;
+	private Scene escenaAnterior;
+
+	public PanelImpresionVolver(Documento doc, double duracion, Stage stage, Scene escenaAnterior) {
+		this.stage = stage;
+		this.escenaAnterior = escenaAnterior;
+		setCenter(new PanelImpresion(doc, duracion));
+	}
+
+	@Override
+	public void volverPresionado() {
+		stage.setScene(escenaAnterior);
+	}
+
+}
