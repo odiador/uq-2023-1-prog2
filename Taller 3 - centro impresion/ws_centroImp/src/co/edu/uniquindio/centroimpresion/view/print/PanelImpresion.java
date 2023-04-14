ad2d07da6596cb10a0976f316cfae174ed4e30a4
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-14 15:29:45 -0500

    Create PanelImpresion.java
    Added the pane for the printing

---
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/print/PanelImpresion.java
@@ -0,0 +1,25 @@
+package co.edu.uniquindio.centroimpresion.view.print;
+
+import co.edu.uniquindio.centroimpresion.controllers.ControlPrintDoc;
+import co.edu.uniquindio.centroimpresion.model.centro.Documento;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+
+public class PanelImpresion extends BorderPane {
+	private Documento doc;
+	private double letrasSeg;
+
+	public PanelImpresion(Documento doc, double letrasSeg) {
+		this.doc = doc;
+		this.letrasSeg = letrasSeg;
+		initComp();
+	}
+
+	private void initComp() {
+		TextArea textoContenido = new TextArea();
+		textoContenido.setEditable(false);
+		setCenter(textoContenido);
+		new Thread(ControlPrintDoc.generarTareaImpresion(doc.getContenido(), letrasSeg,
+				caracter -> textoContenido.appendText(caracter + ""))).start();
+	}
+}
