892ca3a30da1aba4505e72e95091d8252dfdb665
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-14 17:40:29 -0500

    Implemented see buttons

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/menu/PanelMenuSee.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/menu/PanelMenuSee.java
@@ -1,13 +1,14 @@
 package co.edu.uniquindio.centroimpresion.view.menu;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import co.edu.uniquindio.centroimpresion.model.centro.OpcionObjeto;
 import co.edu.uniquindio.centroimpresion.model.centro.TipoAccion;
 import co.edu.uniquindio.centroimpresion.model.centro.TipoEmpleado;
 import co.edu.uniquindio.centroimpresion.view.custom.PanelMenuOpcionObjetos;
 import co.edu.uniquindio.centroimpresion.view.see.PanelSeeDocs;
+import co.edu.uniquindio.centroimpresion.view.see.PanelSeeImpCartucho;
+import co.edu.uniquindio.centroimpresion.view.see.PanelSeeImpLaser;
 
 public class PanelMenuSee extends PanelMenuOpcionObjetos {
 
@@ -16,15 +17,13 @@ public class PanelMenuSee extends PanelMenuOpcionObjetos {
 	}
 
 	public OpcionObjeto[] generarOpciones(TipoEmpleado tipoEmpleado) {
-		ArrayList<OpcionObjeto> listaOpciones = new ArrayList<OpcionObjeto>(Arrays.asList(OpcionObjeto.values()));
-		listaOpciones.remove(OpcionObjeto.DOCUMENTO_ESPEFICO);
-		listaOpciones.remove(OpcionObjeto.IMPRESORA_CARTUCHO);
-		listaOpciones.remove(OpcionObjeto.IMPRESORA_LASER);
-		if (!tipoEmpleado.puedeVerDocs()) {
-			listaOpciones.remove(OpcionObjeto.DOCUMENTO);
+		ArrayList<OpcionObjeto> listaOpciones = new ArrayList<OpcionObjeto>();
+		if (tipoEmpleado.puedeVerDocs()) {
+			listaOpciones.add(OpcionObjeto.DOCUMENTO);
 		}
-		if (!tipoEmpleado.puedeVerImpresoras()) {
-			listaOpciones.remove(OpcionObjeto.IMPRESORA);
+		if (tipoEmpleado.puedeVerImpresoras()) {
+			listaOpciones.add(OpcionObjeto.IMPRESORA_LASER);
+			listaOpciones.add(OpcionObjeto.IMPRESORA_CARTUCHO);
 		}
 		return listaOpciones.toArray(new OpcionObjeto[listaOpciones.size()]);
 	}
@@ -36,10 +35,12 @@ public class PanelMenuSee extends PanelMenuOpcionObjetos {
 
 	@Override
 	public void btnImpCartuPresionado() {
+		setCenter(new PanelSeeImpCartucho(this));
 	}
 
 	@Override
 	public void btnImpLaserPresionado() {
+		setCenter(new PanelSeeImpLaser(this));
 	}
 
 	@Override
