b180fe09c7c9de0acdbc265e256c73d857dc09b6
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-14 16:14:35 -0500

    Renamed PanelSeeImps
    PanelSeeImps -> PanelSeeImpCartucho

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/see/PanelSeeImpCartucho.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/see/PanelSeeImpCartucho.java
@@ -3,15 +3,15 @@ package co.edu.uniquindio.centroimpresion.view.see;
 import co.edu.uniquindio.centroimpresion.view.custom.PanelConVolver;
 import co.edu.uniquindio.centroimpresion.view.custom.PanelMenuOpcionObjetos;
 
-public class PanelSeeImps extends PanelConVolver {
+public class PanelSeeImpCartucho extends PanelConVolver {
 	private PanelMenuOpcionObjetos panel;
 
-	public PanelSeeImps(PanelMenuOpcionObjetos panel) {
+	public PanelSeeImpCartucho(PanelMenuOpcionObjetos panel) {
 		this.panel = panel;
 	}
 
-	@Override
 	public void volverPresionado() {
 		panel.initComp();
 	}
+
 }
