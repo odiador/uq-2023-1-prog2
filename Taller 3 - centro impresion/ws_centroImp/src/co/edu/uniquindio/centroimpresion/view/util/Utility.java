a1f2617c407f2a89d8f8e5cc7587ae928f829803
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-14 18:59:26 -0500

    Created and updated some methods

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/util/Utility.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/util/Utility.java
@@ -1,12 +1,19 @@
 package co.edu.uniquindio.centroimpresion.view.util;
 
 import co.edu.uniquindio.centroimpresion.exceptions.TextIsEmptyException;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Side;
 import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 public class Utility {
 
@@ -15,9 +22,36 @@ public class Utility {
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 				if (!newValue.matches("\\d*")) {
 					tf.setText(newValue.replaceAll("[^\\d]", ""));
+					final ContextMenu menu = new ContextMenu();
+					menu.getItems().add(new MenuItem("Este campo solo\npuede tener numeros"));
+					menu.show(tf, Side.BOTTOM, 0, 0);
+
+					Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), evento -> {
+						menu.hide();
+					}));
+					timeline.play();
 				}
 			}
 		});
+	}
+
+	public static void setMaximumTextLength(TextField tf, int tamanio) {
+		tf.setTextFormatter(new TextFormatter<String>(cambio -> {
+			if (cambio.isContentChange()) {
+				if (cambio.getControlNewText().length() > tamanio) {
+					final ContextMenu menu = new ContextMenu();
+					menu.getItems().add(new MenuItem("Este campo tiene maximo \n" + tamanio + " caracteres"));
+					menu.show(cambio.getControl(), Side.BOTTOM, 0, 0);
+
+					Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), evento -> {
+						menu.hide();
+					}));
+					timeline.play();
+					return null;
+				}
+			}
+			return cambio;
+		}));
 	}
 
 	public static HBox generarHBox(String msg, Node... nodos) {
@@ -45,7 +79,7 @@ public class Utility {
 		} else {
 			concatenacion += inicial + (fainal.isEmpty() ? "" : "." + fainal);
 		}
-	
+
 		return concatenacion;
 	}
 
