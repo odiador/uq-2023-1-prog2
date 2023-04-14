a91e9578626ba22902fc3dcc3957aee64774d4a5
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-14 15:08:57 -0500

    Create NoHayColaImpresionException.java

---
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/exceptions/NoHayColaImpresionException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.centroimpresion.exceptions;
+
+public class NoHayColaImpresionException extends Exception {
+	public NoHayColaImpresionException() {
+		super("No hay cola de impresion");
+	}
+}
