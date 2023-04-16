2f495abd77b738414fdb6f3e6d77bb6f377cb20d
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-15 21:19:28 -0500

    Update TipoEmpleado.java
    Added 2 new attributes

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/centro/TipoEmpleado.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/centro/TipoEmpleado.java
@@ -1,8 +1,10 @@
 package co.edu.uniquindio.centroimpresion.model.centro;
 
 public enum TipoEmpleado {
-	ADMINISTRADOR("Administrador", true, true, true, true, true, true), SUPERVISOR("Supervisor", false, true, false,
-			true, true, true), TRABAJADOR("Trabajador", false, false, false, false, false, false);
+	ADMINISTRADOR("Administrador", true, true, true, true, true, true, true, true),
+	SUPERVISOR("Supervisor", false, true, false, true, true, true, false, true),
+	TRABAJADOR("Trabajador", false, false, false, false, false, false, false, false);
+
 	private boolean puedeAgregarImpresora;
 	private boolean puedeEliminarDocumentos;
 	private boolean puedeEliminarImpresoras;
@@ -10,10 +12,13 @@ public enum TipoEmpleado {
 	private boolean puedeVerDocs;
 	private boolean puedeImprimirDocEsprcifico;
 	private String text;
+	private boolean puedeSeleccionarImpresora;
+	private boolean puedePuedeActualizarDocumento;
 
 	private TipoEmpleado(String text, boolean puedeAgregarImpresora, boolean puedeEliminarDocumentos,
 			boolean puedeEliminarImpresoras, boolean puedeVerImpresoras, boolean puedeVerDocs,
-			boolean puedeImprimirDocEsprcifico) {
+			boolean puedeImprimirDocEsprcifico, boolean puedePuedeActualizarDocumento,
+			boolean puedeSeleccionarImpresora) {
 		this.text = text;
 		this.puedeAgregarImpresora = puedeAgregarImpresora;
 		this.puedeEliminarDocumentos = puedeEliminarDocumentos;
@@ -21,6 +26,8 @@ public enum TipoEmpleado {
 		this.puedeVerImpresoras = puedeVerImpresoras;
 		this.puedeVerDocs = puedeVerDocs;
 		this.puedeImprimirDocEsprcifico = puedeImprimirDocEsprcifico;
+		this.puedePuedeActualizarDocumento = puedePuedeActualizarDocumento;
+		this.puedeSeleccionarImpresora = puedeSeleccionarImpresora;
 	}
 
 	public String getText() {
@@ -49,5 +56,13 @@ public enum TipoEmpleado {
 
 	public boolean puedeImprimirDocEspecifico() {
 		return puedeImprimirDocEsprcifico;
+	}
+
+	public boolean puedeSeleccionarImpresora() {
+		return puedeSeleccionarImpresora;
+	}
+
+	public boolean puedePuedeActualizarDocumento() {
+		return puedePuedeActualizarDocumento;
 	}
 }
