33377857569b0896aa40372759a522851f870265
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-15 21:16:32 -0500

    Changed an attribute type
    Set<Impresora> listaImpresoras -> List<Impresora> listaImpresoras
    And added some utility methods

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/centro/CentroImpresion.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/model/centro/CentroImpresion.java
@@ -6,19 +6,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import co.edu.uniquindio.centroimpresion.exceptions.CentroImpresionException;
 import co.edu.uniquindio.centroimpresion.exceptions.ImpresoraException;
 import co.edu.uniquindio.centroimpresion.exceptions.NoHayColaImpresionException;
+import co.edu.uniquindio.centroimpresion.exceptions.ObjectNotExists;
+import co.edu.uniquindio.centroimpresion.exceptions.TextIsEmptyException;
 
 public class CentroImpresion implements Serializable {
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = -2839899210301744900L;
-	private final Set<Impresora> listaImpresoras = new HashSet<Impresora>();
+	private final List<Impresora> listaImpresoras = new ArrayList<Impresora>();
 	private final List<Documento> listaDocumentos = new ArrayList<Documento>();
 	private final List<Documento> listaDocumentosImpresos = new ArrayList<Documento>();
 
@@ -53,6 +54,12 @@ public class CentroImpresion implements Serializable {
 
 	public Documento buscarDocumento(String code) {
 		return listaDocumentos.stream().filter(doc -> doc.getCode().equals(code)).findAny().orElse(null);
+	}
+
+	public Documento buscarDocumentoThrows(String code) throws ObjectNotExists {
+		return listaDocumentos.stream().filter(doc -> doc.getCode().equals(code)).findAny().orElseThrow(() -> {
+			return new ObjectNotExists(Documento.class);
+		});
 	}
 
 	public boolean validarDocumento(String code) {
@@ -93,6 +100,11 @@ public class CentroImpresion implements Serializable {
 	public void deleteImpresora(String code) throws CentroImpresionException {
 		if (!listaImpresoras.remove(buscarImpresora(code)))
 			throw new CentroImpresionException("La impresora no fue encontrada", Impresora.class);
+	}
+
+	public void actualizarDocumento(Documento documento) throws CentroImpresionException {
+		deleteDocumento(documento.getCode());
+		listaDocumentos.add(documento);
 	}
 
 	public void actualizarImpresora(Impresora impresora) throws CentroImpresionException {
@@ -165,7 +177,7 @@ public class CentroImpresion implements Serializable {
 			throw new CentroImpresionException("La impresora ya existe", impresora);
 	}
 
-	public Set<Impresora> getListaImpresoras() {
+	public List<Impresora> getListaImpresoras() {
 		return listaImpresoras;
 	}
 
@@ -181,6 +193,25 @@ public class CentroImpresion implements Serializable {
 
 	public List<Documento> getListaDocumentos() {
 		return listaDocumentos;
+	}
+
+	public void recargarImpresora(String code) throws CentroImpresionException {
+		Impresora impresora = buscarImpresora(code);
+		if (impresora == null)
+			throw new CentroImpresionException("La impresora no fue encontrada", Impresora.class);
+		impresora.recargar();
+	}
+
+	public void seleccionarImpresora(String code) throws CentroImpresionException {
+		Impresora impresora = buscarImpresora(code);
+		if (impresora == null)
+			throw new CentroImpresionException("La impresora no fue encontrada", Impresora.class);
+		int indiceImpresora = listaImpresoras.indexOf(impresora);
+		if (indiceImpresora == 0)
+			throw new CentroImpresionException("La impresora ya esa seleccionada", impresora);
+		Impresora impresora2 = listaImpresoras.get(0);
+		listaImpresoras.set(0, impresora);
+		listaImpresoras.set(indiceImpresora, impresora2);
 	}
 
 }
