53a9a8c9a767675fab2d77a37942e85e0a11ec87
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-15 22:54:43 -0500

    Corrected method

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/exceptions/ArchivoNoObtenidoException.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/exceptions/ArchivoNoObtenidoException.java
@@ -7,5 +7,6 @@ public class ArchivoNoObtenidoException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public ArchivoNoObtenidoException() {
+		super("El archivo no fue obtenido");
 	}
 }
