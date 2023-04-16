9fcf3329f321fb60220ebb0a3f6f1724e34e8915
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-15 21:41:44 -0500

    Created a new custom button class
    Subsequently the buttons will be replaced with that button class

---
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/custom/Boton.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.centroimpresion.view.custom;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+public class Boton extends BorderPane {
+	public Boton(String text, EventHandler<? super MouseEvent> listener) {
+		Label label = new Label(text);
+		label.setId("btn");
+		label.setOnMouseReleased(listener);
+
+		setId("btn-case");
+		setCenter(label);
+	}
+}
