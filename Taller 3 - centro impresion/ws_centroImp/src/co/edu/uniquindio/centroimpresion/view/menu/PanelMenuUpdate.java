e99e9f1405e3b7c8774304ec773a95fd82c66e32
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-16 00:16:58 -0500

    Implemented panes

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/menu/PanelMenuUpdate.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/menu/PanelMenuUpdate.java
@@ -2,6 +2,10 @@ package co.edu.uniquindio.centroimpresion.view.menu;
 
 import co.edu.uniquindio.centroimpresion.model.centro.TipoEmpleado;
 import co.edu.uniquindio.centroimpresion.view.custom.Boton;
+import co.edu.uniquindio.centroimpresion.view.herramientas.PanelActualizarDoc;
+import co.edu.uniquindio.centroimpresion.view.herramientas.PanelCambiarEstado;
+import co.edu.uniquindio.centroimpresion.view.herramientas.PanelRellenarImp;
+import javafx.geometry.Insets;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
@@ -14,19 +18,37 @@ public class PanelMenuUpdate extends BorderPane {
 	}
 
 	public void initComponents() {
-		VBox vbox = new VBox(30);
+		VBox vbox = new VBox();
 
 		vbox.setId("centered-box");
-		vbox.getChildren().add(new Boton("Rellenar", e -> {
-		}, "boton-opcion"));
+		Boton botonRecargar = new Boton("Recargar Impresora", e -> {
+			setCenter(new PanelRellenarImp(this));
+		}, "boton-opcion");
+
+		vbox.getChildren().add(botonRecargar);
+
+		VBox.setMargin(botonRecargar, new Insets(20));
+
 		if (tipoEmpleado.puedeActualizarDocumento()) {
-			vbox.getChildren().add(new Boton("Actualizar Doc", e -> {
-			}, "boton-opcion"));
+			Boton botonActualizar = new Boton("Actualizar Doc", e -> {
+				setCenter(new PanelActualizarDoc(this));
+			}, "boton-opcion");
+
+			vbox.getChildren().add(botonActualizar);
+			VBox.setMargin(botonActualizar, new Insets(20));
 		}
 		if (tipoEmpleado.puedeSeleccionarImpresora()) {
-			vbox.getChildren().add(new Boton("Seleccionar Impresora", e -> {
-			}, "boton-opcion"));
+			Boton botonSeleccionar = new Boton("Seleccionar Impresora", e -> {
+				setCenter(new PanelActualizarDoc(this));
+			}, "boton-opcion");
+			vbox.getChildren().add(botonSeleccionar);
+			VBox.setMargin(botonSeleccionar, new Insets(20));
 		}
+		Boton botonCambiarEstado = new Boton("Cambiar Estado a Impresora", e -> {
+			setCenter(new PanelCambiarEstado(this));
+		}, "boton-opcion");
+		vbox.getChildren().add(botonCambiarEstado);
+		VBox.setMargin(botonCambiarEstado, new Insets(20));
 		setCenter(vbox);
 	}
 }
