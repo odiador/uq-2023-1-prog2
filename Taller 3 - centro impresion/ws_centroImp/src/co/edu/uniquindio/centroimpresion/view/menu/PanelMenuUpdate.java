3b1c5bb9006999395bc99b6ad4035bb3cf9c5221
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-15 23:16:11 -0500

    Implemented pane design

--- Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/menu/PanelMenuUpdate.java
+++ Taller 3 - centro impresion/ws_centroImp/src/co/edu/uniquindio/centroimpresion/view/menu/PanelMenuUpdate.java
@@ -1,7 +1,9 @@
 package co.edu.uniquindio.centroimpresion.view.menu;
 
 import co.edu.uniquindio.centroimpresion.model.centro.TipoEmpleado;
+import co.edu.uniquindio.centroimpresion.view.custom.Boton;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class PanelMenuUpdate extends BorderPane {
 	private TipoEmpleado tipoEmpleado;
@@ -12,6 +14,19 @@ public class PanelMenuUpdate extends BorderPane {
 	}
 
 	public void initComponents() {
+		VBox vbox = new VBox(30);
 
+		vbox.setId("centered-box");
+		vbox.getChildren().add(new Boton("Rellenar", e -> {
+		}, "boton-opcion"));
+		if (tipoEmpleado.puedeActualizarDocumento()) {
+			vbox.getChildren().add(new Boton("Actualizar Doc", e -> {
+			}, "boton-opcion"));
+		}
+		if (tipoEmpleado.puedeSeleccionarImpresora()) {
+			vbox.getChildren().add(new Boton("Seleccionar Impresora", e -> {
+			}, "boton-opcion"));
+		}
+		setCenter(vbox);
 	}
 }
