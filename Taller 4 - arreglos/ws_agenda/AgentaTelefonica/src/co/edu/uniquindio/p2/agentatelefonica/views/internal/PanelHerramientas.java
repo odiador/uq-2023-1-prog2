9b96672bedda90f2392644eb9aa7249660e6cdc8
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-04-21 16:18:50 -0500

    Moved package route

---
+++ Taller 4 - arreglos/ws_agenda/AgentaTelefonica/src/co/edu/uniquindio/p2/agentatelefonica/views/internal/PanelHerramientas.java
@@ -0,0 +1,42 @@
+package co.edu.uniquindio.p2.agentatelefonica.views.internal;
+
+import co.edu.uniquindio.p2.agentatelefonica.controllers.CtrlAgenda;
+import co.edu.uniquindio.p2.agentatelefonica.util.Boton;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class PanelHerramientas extends BorderPane {
+	private EventHandler<? super MouseEvent> eventoVolver;
+
+	public PanelHerramientas(EventHandler<? super MouseEvent> eventoVolver) {
+		this.eventoVolver = eventoVolver;
+		initComponents();
+	}
+
+	private void initComponents() {
+		VBox vbox = new VBox(20);
+		Boton btnListar = new Boton("Listar Contactos", e -> {
+			setCenter(new PanelVerContactos());
+			setBottom(new Boton("Volver", evtVolver2 -> {
+				initComponents();
+			}, "btn-volver"));
+		});
+		Boton btnHuecosLibres = new Boton("Indicar Huecos libres", e -> {
+			CtrlAgenda.huecosLibres();
+		});
+		vbox.setId("centered-box");
+		vbox.getChildren().add(btnHuecosLibres);
+		vbox.getChildren().add(btnListar);
+
+		Insets instets = new Insets(0, 80, 0, 80);
+
+		VBox.setMargin(btnHuecosLibres, instets);
+		VBox.setMargin(btnListar, instets);
+		Boton botonVolver = new Boton("Volver", eventoVolver, "btn-volver");
+		setCenter(vbox);
+		setBottom(botonVolver);
+	}
+}
